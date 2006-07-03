7c970a7d1ffac37d3782656fe09e40ccc4a519ff
Michael Norrish <Michael.Norrish@nicta.com.au>
2006-07-03 01:56:55 +0000

    Made deliverable manufacture controlled by shell-script rather than Makefile.

---
+++ make-deliverable1.sh
@@ -0,0 +1,21 @@
+cd /tmp
+/bin/rm -rf qinetiq-cpp
+cvs -d ~/repository export -D "2006-07-03 02:00Z" qinetiq-cpp
+cd qinetiq-cpp
+cd docs
+pdflatex deliverable1-notes
+bibtex deliverable1-notes
+pdflatex deliverable1-notes
+pdflatex deliverable1-notes
+/bin/rm deliverable1-notes.{bbl,blg,aux,log}
+
+cd .. ; /bin/rm make-deliverable1.sh
+
+cd talks
+latex darp2006
+latex darp2006
+dvips -Ppdf darp2006
+rm darp2006.{aux,dvi,log,nav,out,ps,snm,toc}
+
+cd ../..
+tar cvzf deliverable1.tgz qinetiq-cpp
