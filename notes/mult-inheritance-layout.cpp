f15c59b72f6d6def107b6b7d172a1b30f45a9780
Michael Norrish <Michael.Norrish@nicta.com.au>
2006-07-21 03:35:05 +0000

    Notes on g++ experiments I have performed, and source code for one of them.

---
+++ notes/mult-inheritance-layout.cpp
@@ -0,0 +1,53 @@
+#include <cstdio>
+
+class B {
+public:
+  int b;
+};
+
+class D1 : virtual public B {
+public:
+  int x;
+};
+
+class D2 : virtual public B {
+public:
+  int y;
+};
+
+class D3 : virtual public B {
+public:
+  int z;
+};
+
+class E : public D1, public D2, public D3 {
+public:
+  int e;
+};
+
+void print_layout(unsigned char *p, unsigned sz)
+{
+  for (unsigned i = 0; i < sz; i++)
+    printf("%2d (%p): %02x\n", i, p + i, p[i]);
+}
+
+
+
+int main(void)
+{
+  E e;
+  e.e = 9;
+  e.b = 4;
+  e.x = 1;
+  e.y = 2;
+  e.z = 3;
+  printf("*** An E\n");
+  print_layout((unsigned char *)(&e), sizeof(e));
+  D1 d;
+  d.x = 1;
+  d.b = 7;
+  printf("*** A D1\n");
+  print_layout((unsigned char *)(&d), sizeof(d));
+  return 0;
+}
+
